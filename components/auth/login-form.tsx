486f6b58ae0140a6e5a6ac4d1c879f70ed35d8ab
Charles <c_ukasoanya@yahoo.com>
2024-01-14 21:18:47 +0100

    added google and github OAuth functionality

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -23,8 +23,15 @@ import { Button } from '../ui/button';
 import { FormError } from '../form-error';
 import { FormSuccess } from '../form-success';
 import { login } from '@/actions/login';
+import { useSearchParams } from 'next/navigation';
 
 export const LoginForm = () => {
+  const searchParams = useSearchParams();
+  const urlError =
+    searchParams.get('error') === 'OAuthAccountNotLinked'
+      ? 'Email already in use with a different provider!'
+      : '';
+
   const [isPending, startTransition] = useTransition();
 
   const [error, setError] = useState<string | undefined>('');
@@ -45,8 +52,8 @@ export const LoginForm = () => {
 
     startTransition(() => {
       login(values).then((data) => {
-        setError(data.error);
-        setSuccess(data.success);
+        setError(data?.error);
+        // setSuccess(data?.success);
       });
     });
   };
@@ -99,7 +106,7 @@ export const LoginForm = () => {
             />
           </div>
 
-          <FormError message={error} />
+          <FormError message={error || urlError} />
           <FormSuccess message={success} />
 
           <Button type="submit" className="w-full" disabled={isPending}>
